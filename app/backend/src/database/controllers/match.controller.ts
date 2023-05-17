fbf9801e24579c75858655766cf4df0ec5828d2e
luc0liv <luoliveiradacunha@gmail.com>
2023-05-17 20:25:11 -0300

    feat: create endpoint to filter matches

--- app/backend/src/database/controllers/match.controller.ts
+++ app/backend/src/database/controllers/match.controller.ts
@@ -3,10 +3,15 @@ import statusCodes from '../../utils/statusCodes';
 import MatchService from '../services/match.service';
 
 export default class MatchController {
-  static async getAllMatchs(_req: Request, res: Response, next: NextFunction) {
+  static async getAllMatchs(req: Request, res: Response, next: NextFunction) {
     try {
-      const matchs = await MatchService.getAllMatchs();
-      return res.status(statusCodes.ok).json(matchs);
+      const { inProgress } = req.query;
+      if (inProgress) {
+        const filteredMatches = await MatchService.getMatchesByProgress(inProgress as string);
+        return res.status(statusCodes.ok).json(filteredMatches);
+      }
+      const matches = await MatchService.getAllMatches();
+      return res.status(statusCodes.ok).json(matches);
     } catch (error) {
       next(error);
     }
--- app/backend/src/database/services/match.service.ts
+++ app/backend/src/database/services/match.service.ts
@@ -3,7 +3,7 @@ import IMatch from '../../interfaces/Match';
 import Team from '../models/Team';
 
 export default class MatchService {
-  public static async getAllMatchs(): Promise<IMatch[]> {
+  public static async getAllMatches(): Promise<IMatch[]> {
     const matches = await Match.findAll({
       include: [
         {
@@ -17,6 +17,26 @@ export default class MatchService {
           attributes: { exclude: ['id'] },
         },
       ],
+    });
+    return matches;
+  }
+
+  public static async getMatchesByProgress(inProgress: string): Promise<IMatch[]> {
+    const convertedInProgress = (inProgress === 'true');
+    const matches = await Match.findAll({
+      include: [
+        {
+          model: Team,
+          as: 'homeTeam',
+          attributes: { exclude: ['id'] },
+        },
+        {
+          model: Team,
+          as: 'awayTeam',
+          attributes: { exclude: ['id'] },
+        },
+      ],
+      where: { inProgress: convertedInProgress },
     });
     return matches;
   }
