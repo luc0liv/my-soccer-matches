cfe58a15aad7328955cb536ea37fef0f18583e5a
luc0liv <luoliveiradacunha@gmail.com>
2023-05-21 18:07:19 -0300

    feat: add efficiency and goalsBalance

--- app/backend/src/database/services/leaderboardHomeQuery.ts
+++ app/backend/src/database/services/leaderboardHomeQuery.ts
@@ -1,7 +1,11 @@
 export default `
 SELECT 
     teams.team_name AS name,
-    CAST(SUM(matches.home_team_goals) AS UNSIGNED) AS totalPoints,
+    CAST(SUM(CASE 
+        WHEN matches.home_team_goals > matches.away_team_goals THEN 3 ELSE 0 END) AS UNSIGNED) +
+    CAST(SUM(CASE 
+        WHEN matches.home_team_goals = matches.away_team_goals THEN 1 ELSE 0 END) AS UNSIGNED)
+    AS totalPoints,
     COUNT(teams.team_name) AS totalGames,
     CAST(SUM(CASE
             WHEN matches.home_team_goals > matches.away_team_goals THEN 1
@@ -20,20 +24,24 @@ SELECT
         AS UNSIGNED) AS totalLosses,
     CAST(SUM(matches.home_team_goals) AS UNSIGNED) AS goalsFavor,
     CAST(SUM(matches.away_team_goals) AS UNSIGNED) AS goalsOwn,
-    CAST(SUM(matches.home_team_goals - matches.away_team_goals)
-        AS SIGNED) AS goalsBalance
+    CAST(SUM(matches.home_team_goals) - SUM(matches.away_team_goals)
+        AS SIGNED) AS goalsBalance,
+ROUND(
+    SUM(CASE 
+    WHEN matches.home_team_goals > matches.away_team_goals THEN 3
+    WHEN matches.home_team_goals = matches.away_team_goals THEN 1 ELSE 0
+    END
+) / ((COUNT(teams.team_name) * 3)) * 100, 2
+    ) AS efficiency
 FROM
     TRYBE_FUTEBOL_CLUBE.teams AS teams
         INNER JOIN
     TRYBE_FUTEBOL_CLUBE.matches AS matches ON matches.home_team_id = teams.id
 WHERE
-    matches.in_progress = FALSE
-        AND matches.home_team_goals > matches.away_team_goals
+    matches.in_progress = 0
 GROUP BY teams.team_name
-ORDER BY 
- totalPoints DESC ,
- totalVictories DESC , 
- goalsBalance DESC , 
- goalsFavor DESC , 
- goalsOwn DESC;
-`;
+ORDER BY totalPoints DESC ,
+ totalVictories DESC ,
+ goalsBalance DESC ,
+  goalsFavor DESC ,
+   goalsOwn DESC;`;
