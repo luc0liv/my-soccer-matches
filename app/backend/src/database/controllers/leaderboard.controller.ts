e7a33cc2f80f4354a6e57836de9b68278804b2bb
luc0liv <luoliveiradacunha@gmail.com>
2023-05-21 22:45:48 -0300

    refactor: change path validation

--- app/backend/src/database/controllers/leaderboard.controller.ts
+++ app/backend/src/database/controllers/leaderboard.controller.ts
@@ -1,19 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 import statusCodes from '../../utils/statusCodes';
 import LeaderboardService from '../services/leaderboard.service';
-import leaderboardAwayQuery from '../../utils/leaderboardAwayQuery';
-import leaderboardHomeQuery from '../../utils/leaderboardHomeQuery';
 
 export default class LeaderboardController {
   static async getLeaderboard(req: Request, res: Response, next: NextFunction) {
     try {
-      let query;
-      if (req.path.includes('home')) {
-        query = leaderboardHomeQuery;
-      } else {
-        query = leaderboardAwayQuery;
-      }
-      const leaderboard = await LeaderboardService.getLeaderboard(query);
+      const leaderboard = await LeaderboardService.getLeaderboard(req.path);
       return res.status(statusCodes.ok).json(leaderboard);
     } catch (error) {
       next(error);
--- app/backend/src/database/services/leaderboard.service.ts
+++ app/backend/src/database/services/leaderboard.service.ts
@@ -1,8 +1,17 @@
 import sequelize from '../models/index';
 import Leaderboard from '../../interfaces/Leaderboard';
+import leaderboardAwayQuery from '../../utils/leaderboardAwayQuery';
+import leaderboardHomeQuery from '../../utils/leaderboardHomeQuery';
 
 export default class LeaderboardService {
-  public static async getLeaderboard(query:string): Promise<Leaderboard[]> {
+  public static async getLeaderboard(path: string): Promise<Leaderboard[]> {
+    let query = '';
+    if (path.includes('home')) {
+      query = leaderboardHomeQuery;
+    }
+    if (path.includes('away')) {
+      query = leaderboardAwayQuery;
+    }
     const [leaderboard] = await sequelize.query(query);
     return leaderboard as Leaderboard[];
   }
